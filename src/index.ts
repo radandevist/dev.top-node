cd31afb196c1e83c506f7d1915cdb982dd1d89a2
Andrianarisoa Daniel <radandevist@gmail.com>
2022-11-09 09:38:27 +0300

    🎨 swc config, nodemon config & first endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log("Hello world");
+import express from "express"
+
+const app = express()
+const port = 3300
+
+app.use("/api", function (req, res) {
+  res.send({ message: "welcome to our api" })
+})
+
+app.listen(port, function () {
+  console.log("App is running on port " + port)
+})
