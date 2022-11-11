75b23354017a33f9b343df45900753dc4ed23336
Andrianarisoa Daniel <radandevist@gmail.com>
2022-11-11 10:31:06 +0300

    🗃️ enabled user entity and synchronize dataSource

--- src/dataSource.ts
+++ src/dataSource.ts
@@ -1,9 +1,11 @@
+import { User } from './user.entity'
 import { DataSource } from "typeorm"
 
 const { 
   DB_USER,
   DB_PASSWORD,
-  DB_NAME
+  DB_NAME,
+  NODE_ENV,
 } = process.env;
 
 export const dataSource = new DataSource({
@@ -13,6 +15,8 @@ export const dataSource = new DataSource({
     username: DB_USER,
     password: DB_PASSWORD,
     database: DB_NAME,
+    entities: [User],
+    synchronize: NODE_ENV !== "production",
 })
 
 export function initializeDataSource() {
