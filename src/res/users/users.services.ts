dde502a854562bdb632efcd378d23c95ea6ee4ec
Andrianarisoa Daniel <radandevist@gmail.com>
2023-03-14 07:55:30 +0300

    ♻️ auth services converted to prisma

--- src/res/users/users.services.ts
+++ src/res/users/users.services.ts
@@ -1,13 +1,17 @@
-import { usersRepository } from "../../infra/dataSource";
+// import { usersRepository } from "../../infra/dataSource";
+import { prisma } from "../../infra/prisma";
 
 export async function findUserByEmail(email: string) {
-  return usersRepository.findOneBy({ email });
+  // return usersRepository.findOneBy({ email });
+  return prisma.user.findUnique({ where: { email } });
 }
 
 export async function findUserByUserName(userName: string) {
-  return usersRepository.findOneBy({ userName });
+  // return usersRepository.findOneBy({ userName });
+  return prisma.user.findUnique({ where: { userName } });
 }
 
 export async function findUserById(id: string) {
-  return usersRepository.findOneBy({ id });
+  // return usersRepository.findOneBy({ id });
+  return prisma.user.findUnique({ where: { id } });
 }
