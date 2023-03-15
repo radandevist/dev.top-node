26887796953deeadb3680d1a05f2784771ba5d0d
Andrianarisoa Daniel <radandevist@gmail.com>
2023-03-15 10:18:55 +0300

    ➖ removed unused files

--- src/res/comments/comments.entity.ts
+++ src/res/comments/comments.entity.ts
@@ -1,32 +1,32 @@
-import typeorm, {
-  Column,
-  CreateDateColumn,
-  Entity,
-  ManyToOne,
-  PrimaryGeneratedColumn,
-  UpdateDateColumn,
-} from "typeorm";
+// import typeorm, {
+//   Column,
+//   CreateDateColumn,
+//   Entity,
+//   ManyToOne,
+//   PrimaryGeneratedColumn,
+//   UpdateDateColumn,
+// } from "typeorm";
 
-import { Post } from "../posts/posts.entity";
-import { User } from "../users/users.entity";
+// import { Post } from "../posts/posts.entity";
+// import { User } from "../users/users.entity";
 
-@Entity()
-export class Comment {
-  @PrimaryGeneratedColumn("uuid")
-    id!: string;
+// @Entity()
+// export class Comment {
+//   @PrimaryGeneratedColumn("uuid")
+//     id!: string;
 
-  @Column({ type: "longtext" })
-    content!: string;
+//   @Column({ type: "longtext" })
+//     content!: string;
 
-  @ManyToOne(() => User, (user) => user.comments)
-    author!: typeorm.Relation<User>;
+//   @ManyToOne(() => User, (user) => user.comments)
+//     author!: typeorm.Relation<User>;
 
-  @ManyToOne(() => Post, (post) => post.comments)
-    post!: typeorm.Relation<Post>;
+//   @ManyToOne(() => Post, (post) => post.comments)
+//     post!: typeorm.Relation<Post>;
 
-  @CreateDateColumn()
-    createdAt!: Date;
+//   @CreateDateColumn()
+//     createdAt!: Date;
 
-  @UpdateDateColumn()
-    updatedAt!: Date;
-}
+//   @UpdateDateColumn()
+//     updatedAt!: Date;
+// }
