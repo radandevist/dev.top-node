ff7ce250c2979fabb53ea474ccdbedd3683f1f19
Andrianarisoa Daniel <radandevist@gmail.com>
2023-03-17 08:41:16 +0300

    🐛 adjust validateResources middleware

--- src/middlewares/validateResource.ts
+++ src/middlewares/validateResource.ts
@@ -6,11 +6,16 @@ import { AppError } from "../classes/AppError";
 export function validateResource(schema: AnyZodObject) {
   return (req: Request, _res: Response, next: NextFunction) => {
     try {
-      schema.parse({
+      const result = schema.parse({
         body: req.body,
         query: req.query,
         params: req.params,
       });
+
+      req.body = result.body;
+      req.query = result.query;
+      req.params = result.params;
+
       next();
     } catch (error: any) {
       if (error instanceof ZodError) {
